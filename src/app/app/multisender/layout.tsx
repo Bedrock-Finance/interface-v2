2fbd29a0d90627d1d3d42a6ea7c4bd0d1e9f2448
henrygagnier <prosock@protonmail.com>
2024-01-10 20:52:40 -0500

    feat: add multisender seo

---
+++ src/app/app/multisender/layout.tsx
@@ -0,0 +1,9 @@
+import { Metadata } from "next";
+import Multisender from "./page";
+
+export const metadata: Metadata = {
+  title: '100% Free Multisender | BedrockMint',
+  description:
+    "Send coins and tokens all in one transaction to save gas for free.",
+};
+export default Multisender;

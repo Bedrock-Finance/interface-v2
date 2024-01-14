ad0d9a6225bffabdf0fef9bde749a921593822c8
henrygagnier <prosock@protonmail.com>
2024-01-13 19:28:59 -0500

    fix: add &apos;

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -9,7 +9,7 @@ export default function Blog() {
             <p className={styles.blogHeader}>News</p>
             <div className={styles.arcticle}>
                 <p className={styles.title}>Bedrock Finance Officially Launches Blog</p>
-                <p className={styles.arcticleDesc}>To expand Bedrock Finance's impact and reach for news, we officially launch our blog with all updates of Bedrock Finance.</p>
+                <p className={styles.arcticleDesc}>To expand Bedrock Finance&apos;s impact and reach for news, we officially launch our blog with all updates of Bedrock Finance.</p>
                 <p className={styles.arcticleDetails}>Henry - January 13th, 2023</p>
             </div>
         </div>

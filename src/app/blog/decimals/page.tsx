2adac499646e515fec119b38763bd12a360faed2
henrygagnier <prosock@protonmail.com>
2024-01-16 16:35:58 -0500

    fix: change "'" to "&apos;"

--- src/app/blog/decimals/page.tsx
+++ src/app/blog/decimals/page.tsx
@@ -17,7 +17,7 @@ export default function Arcticle() {
             <div className={styles.articleContent}>
             <p className={styles.sectionTitle}>What are decimals in crypto?</p>
             <p>Decimals are a unit for cryptocurrencies to measure the minimum divisible amount of a certain cryptocurrency. For example, the United States Dollar (USD) has two decimals. This means the minimum divisible amount is 0.01 USD. You can also think of decimals as the amount of decimals places the minimum divisive share is.</p>
-            <p>Bitcoin has 8 decimals, with the minimum division being a satoshi (named after bitcoin’s developer, Satoshi Nakamoto), or  0.00000001 BTC.</p>
+            <p>Bitcoin has 8 decimals, with the minimum division being a satoshi (named after bitcoin&apos;s developer, Satoshi Nakamoto), or  0.00000001 BTC.</p>
             <p>Ethereum has 18 decimals, with the minimum division being a wei (named after Wei Dai, an influential cryptographer), or  0.000000000000000001 ETH.</p>
             <p className={styles.sectionTitle}>What should I make my tokens decimals?</p>
             <p>Decimals can be made from 0-18, meaning the smallest division of your token could be 1 or 0.000000000000000001. Having smaller decimals can make it harder for people to transfer small amounts of your token, especially if the price is high or the supply is low. Some external integrations can break if decimals are a number other than 18, as they assume 18 decimal places.</p> 
